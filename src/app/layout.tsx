e97aad8448037a87f16dc727d4972b4020b783cc
Your Name <youremail@example.com>
2025-03-16 21:44:17 +0600

    install and design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/Providers";
-
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
      <Providers>
        <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          {children}
+        <AntdRegistry>{children}</AntdRegistry>
         </body>
       </html>
      </Providers>
